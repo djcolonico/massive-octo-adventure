bcee2df5b0da388ee843f34a397aaf1a2114cf6b
Dylan <dylanjcolonico@gmail.com>
2012-10-14 16:31:55 +0800

    PUSH UP PROJECT

---
+++ UWAnnachat/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UWAnnachat::Application.config.secret_token = 'ae9c561e96ed439550e6c178fef0f014d87c86189df9f3f58ee1e6d9880d4cbe552ac05b1be2c3005f29820b12cf7fa3caff8e1ea85d307f07fbd17d24bc377a'
