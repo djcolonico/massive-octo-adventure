c8dead9d4c91f0258d0207b54c071125c2565267
Erika <oddpuffin@gmail.com>
2012-10-26 01:58:32 +0800

    Static controller + routes

--- UWAnnachat/config/routes.rb
+++ UWAnnachat/config/routes.rb
@@ -5,7 +5,17 @@ UWAnnachat::Application.routes.draw do
   	delete "logout" => :destroy
   end
 
+  controller :static do
+  	get "faq" => :faq
+  	get "rules" => :rules
+  	get "about" => :about
+  	get "sitemap" => :sitemap
+  	get "contact" => :contact
+  end
+
   resources :users
   resources :chatrooms
   resources :messages
+
+  root :to => 'static#home'
 end
