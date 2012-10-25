1e7a449b5a8a16b3d20b0c75974980b678fdaf2d
Dylan <dylanjcolonico@gmail.com>
2012-10-25 18:36:27 +0800

    fixed user model, added password presence, deleted articles and comments test

--- UWAnnachat/test/unit/user_test.rb
+++ UWAnnachat/test/unit/user_test.rb
@@ -4,4 +4,14 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  # validation tests for a new User
+  test "a new model validates pesence of attributes" do
+  	user = User.new
+  	assert user.errors[:name].any?
+  	assert user.errors[:password].any?
+  	assert user.errors[:password_confirmation].any?
+  	assert user.errors[:email].any?
+  end
+
 end
