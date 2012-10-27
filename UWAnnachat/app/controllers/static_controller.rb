5a783c4be395d0a310c7629762b168c975358778
Bob J. M. Hayden <bobjmhayden@gmail.com>
2012-10-27 12:22:19 +0800

    Made users leave chatrooms when changing to static page.

--- UWAnnachat/app/controllers/static_controller.rb
+++ UWAnnachat/app/controllers/static_controller.rb
@@ -2,5 +2,26 @@ class StaticController < ApplicationController
 	skip_before_filter :authorize
 
 	def home
+		if (User.find_by_id(session[:user_id]))
+			User.find(session[:user_id]).update_attribute(:chatroom_id, nil)
+		end
+	end
+
+	def about
+		if (User.find_by_id(session[:user_id]))
+			User.find(session[:user_id]).update_attribute(:chatroom_id, nil)
+		end
+	end
+
+	def rules
+		if (User.find_by_id(session[:user_id]))
+			User.find(session[:user_id]).update_attribute(:chatroom_id, nil)
+		end
+	end
+
+	def faq
+		if (User.find_by_id(session[:user_id]))
+			User.find(session[:user_id]).update_attribute(:chatroom_id, nil)
+		end
 	end
 end
